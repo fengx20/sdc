bce040deed9ee8c3917931fa81d4d0b6c1ca5b68
Fengx <fengx.work@foxmail.com>
2023-07-08 18:34:02 +0800

    项目初始化

---
+++ src/main/java/com/fengx/music/sdc/SdcApplication.java
@@ -0,0 +1,13 @@
+package com.fengx.music.sdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengx/music/sdc/SdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengx.music.sdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
