f0e3fd503d87f07f9125474c9f7783f9d101832f
fengx <fengx.work@foxmail.com>
2023-08-16 16:28:49 +0800

    项目通用配置类（跨域，MyBatis配置）

---
+++ src/main/java/com/fengx/music/sdc/config/MyBatisConfig.java
@@ -0,0 +1,28 @@
+package com.fengx.music.sdc.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * 描述：MyBatis配置类
+ *
+ * @author fengx
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan({"com.fengx.music.sdc.modules.*.mapper"})
+public class MyBatisConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
