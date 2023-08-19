f96de447514223a7f837234d3380cdf075cea7bb
fengx <fengx.work@foxmail.com>
2023-08-19 10:29:38 +0800

    引入MyBatisPlus代码生成器

--- src/main/java/com/fengx/music/sdc/SdcApplication.java
+++ src/main/java/com/fengx/music/sdc/SdcApplication.java
@@ -3,7 +3,13 @@ package com.fengx.music.sdc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 描述：
+ *
+ * @author fengx
+ **/
 @SpringBootApplication
+// @EnableTransactionManagement  //开启事务管理
 public class SdcApplication {
 
     public static void main(String[] args) {
